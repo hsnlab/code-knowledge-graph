a84a6df132019f734bca094c8b8e2feceb827d7e
Vargamatyas <vargamatyas0202@gmail.com>
2025-10-01 23:16:29 +0200

    add: erlang import parsing

--- package/adapters.py
+++ package/adapters.py
@@ -116,6 +116,48 @@ class CppAdapter(LanguageAdapter):
 
         return imports
 
+
+class ErlangAdapter(LanguageAdapter):
+
+    def __init__(self):
+        super().__init__(language="erlang", mapper={
+            "import_attribute": NodeType.IMPORT,  # For actual -import() statements
+            "fun_decl": NodeType.FUNCTION,
+            "function_clause": NodeType.FUNCTION,
+            "call": NodeType.CALL,
+            "remote": NodeType.CALL,
+        })
+
+    def parse_import(self, top_import_node: Node, file_id: str, imp_id: int) -> list[pd.DataFrame]:
+        imports: list = list()
+
+        # Erlang doesn't have 'from' or 'as' - just the module name
+        from_module = None
+        as_name = None
+
+        # For Erlang: -import(lists, [map/2, filter/2]).
+        # We want to extract the module name (first atom child)
+        for child in top_import_node.named_children:
+            name = None
+
+            # The module name is an 'atom' type
+            if child.type == 'atom':
+                name = child.text.decode('utf-8')
+
+                if name:
+                    new_row = pd.DataFrame([{
+                        'file_id': file_id,
+                        'imp_id': imp_id,
+                        'name': name,
+                        'from': from_module,
+                        'as_name': as_name
+                    }])
+                    imports.append(new_row)
+                    # Only get the first atom (module name), ignore the function list
+                    break
+
+        return imports
+
 """
 Mapper to store language-specific adapters and parsers.
 
@@ -127,13 +169,7 @@ Provides:
 adapter_mapper: dict[str, LanguageAdapter] = {
     "python": PythonAdapter(),
     "cpp": CppAdapter(),
-    "erlang": LanguageAdapter(language="erlang", mapper={
-    "import_attribute": NodeType.IMPORT,  # For actual -import() statements
-    "fun_decl": NodeType.FUNCTION,
-    "function_clause": NodeType.FUNCTION,
-    "call": NodeType.CALL,
-    "remote": NodeType.CALL,
-})
+    "erlang": ErlangAdapter()
 }
 
 
